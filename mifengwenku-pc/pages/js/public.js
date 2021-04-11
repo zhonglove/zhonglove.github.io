76b73e3af827946e76332cb87db479855d8623d4
vlily <976651023@qq.com>
2021-04-11 08:12:29 +0800

    1

--- mifengwenku-pc/pages/js/public.js
+++ mifengwenku-pc/pages/js/public.js
@@ -271,12 +271,12 @@
 		return this.each(function(){
 			var _this = $(this);
 			_this.on(options.eventType,options.switchNav,function(){
-				_this.find(options.switchNav).bind(options.eventType, function(){
+				// _this.find(options.switchNav).bind(options.eventType, function(){
 					var _index = $(this).index();
 					$(this).addClass(options.active).siblings(options.switchNav).removeClass(options.active);
 					_this.find(options.switchCont).eq(_index).addClass(options.active).siblings(options.switchCont).removeClass(options.active);
 					_this.find(options.switchMore).eq(_index).addClass(options.activeMore).siblings(options.switchMore).removeClass(options.activeMore);
-				});
+				// });
 			})
 		});
 	};
